b98671f8811a68abc38c6355f84ee0efec8c2adb
David Hughes <dghughes82@gmail.com>
2014-02-11 00:36:42 +0000

    it works - sort of\!

--- GoL.py
+++ GoL.py
@@ -1,10 +1,13 @@
 import numpy
+import os
+import time
+
 
 class Board:
 
-  def __init__(self, rows=8, columns=8, board_layout=None):
+  def __init__(self, rows=16, columns=16, board_layout=None):
     ''' Creating a new Board instance with no arguments
-        creates an 8x8 array of zeroes.
+        creates an 16x16 array of zeroes.
         Alternatively, you can create a specific board layout by inputting
         a Numpy array.
 
@@ -25,7 +28,7 @@ class Board:
     ''' print board layout '''
     print self.board
 
-  def count_adjacent_rows(self, x, y):
+  def count_adjacent_cells(self, x, y):
     ''' dy = -1, dx = -1, 0, 1
         dy =  0, dx = -1, 0, 1
         dy =  1, dx = -1, 0, 1 '''
@@ -43,7 +46,7 @@ class Board:
             if self.board[x+dx][y+dy] == 1:
               #print (x+dx, y+dy, self.board[x][y])
               c = c + 1
-    print (x, y, c)
+    #print (x, y, c)
     return c
 
   def check_action_required(self, x, y):
@@ -53,7 +56,7 @@ class Board:
     Any live cell with more than three live neighbours dies, as if by overcrowding.
     Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
     '''
-    c = self.count_adjacent_rows(x, y)
+    c = self.count_adjacent_cells(x, y)
 
     if self.board[x][y] == 0 and c == 3:
       return 1
@@ -63,42 +66,27 @@ class Board:
       return 1
     if self.board[x][y] == 1 and (c > 3):
       return 0
+    else:
+      return 0
 
 
 def generate_new_board(board):
   new_board_instance = Board()
-  for x in range(0, 8):
-    for y in range(0, 8):
-      new_board_instance[x][y] = board.check_action_required(x, y)
+  for x in range(0, board.rows):
+    for y in range(0, board.columns):
+      new_board_instance.board[x][y] = board.check_action_required(x, y)
   return new_board_instance
 
+board_glider = Board()
 
-new_board = Board()
+board_glider.board[7][4] = 1
+board_glider.board[7][5] = 1
+board_glider.board[7][6] = 1
+board_glider.board[6][6] = 1
+board_glider.board[5][5] = 1
 
-new_board.board[7][4] = 1
-new_board.board[7][5] = 1
-new_board.board[7][6] = 1
-new_board.board[6][6] = 1
-new_board.board[5][5] = 1
-
-
-new_board.printboard()
-new_board = generate_new_board(new_board)
-new_board.printboard()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+while True:
+    os.system('clear') # on Windows you'd use os.system('cls')
+    board_glider.printboard()
+    board_glider = generate_new_board(board_glider)
+    time.sleep(1) 
